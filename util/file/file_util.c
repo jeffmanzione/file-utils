f9496013f2f206e9d8e348c3fbd8fafe832e6ff3
Jeff <jeffreymanzione@gmail.com>
2023-02-20 11:05:25 -0800

    User __errorf instead of __error

--- util/file/file_util.c
+++ util/file/file_util.c
@@ -25,15 +25,15 @@
 FILE *file_fn(const char fn[], const char op_type[], int line_num,
               const char func_name[], const char file_name[]) {
   if (NULL == fn) {
-    __error(line_num, func_name, file_name, "Unable to read file.");
+    __errorf(line_num, func_name, file_name, "Unable to read file.");
   }
   if (NULL == op_type) {
-    __error(line_num, func_name, file_name, "Unable to read file '%s'.", fn);
+    __errorf(line_num, func_name, file_name, "Unable to read file '%s'.", fn);
   }
   FILE *file = fopen(fn, op_type);
   if (NULL == file) {
-    __error(line_num, func_name, file_name,
-            "Unable to read file '%s' with op '%s'.", fn, op_type);
+    __errorf(line_num, func_name, file_name,
+             "Unable to read file '%s' with op '%s'.", fn, op_type);
   }
   return file;
 }
